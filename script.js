0d89036b091c4dfe6dc09e3691af8ff25219f468
Jbeach32 <joshbeach2007@gmail.com>
2023-07-25 16:38:52 -0600

    Gamble in the JS

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-
 $(document).ready(function () {
-    const currentDate = dayjs();
-    const currentHour = currentDate.hour();
+    
+    function createTimeBlocks() {
+      const container = $(".container-lg");
+      const currentHour = dayjs().hour();
+  
+      for (let hour = 9; hour <= 17; hour++) {
+        const timeblock = $("<div>")
+          .attr("id", `hour-${hour}`)
+          .addClass("row time-block")
+          .addClass(hour < currentHour ? "past" : hour === currentHour ? "present" : "future");
+  
+        const hourCol = $("<div>")
+          .addClass("col-2 col-md-1 hour text-center py-3")
+          .text(`${hour > 12 ? hour - 12 : hour}${hour >= 12 ? "PM" : "AM"}`);
+  
+        const descriptionCol = $("<textarea>")
+          .addClass("col-8 col-md-10 description")
+          .attr("rows", "3");
+  
+        const saveBtn = $("<button>")
+          .addClass("btn saveBtn col-2 col-md-1")
+          .attr("aria-label", "save")
+          .html('<i class="fas fa-save" aria-hidden="true"></i>');
+  
+        
+        timeblock.append(hourCol, descriptionCol, saveBtn);
+  
+        
+        container.append(timeblock);
+      }
+    }
   
     
-    $("#currentDay").text(currentDate.format("dddd, MMMM D, YYYY"));
+    createTimeBlocks();
   
     
     function updateTimeblockBackground() {
+      const currentHour = dayjs().hour();
+  
       $(".time-block").each(function () {
         const timeblockHour = parseInt($(this).attr("id").split("-")[1]);
   
+       
         if (timeblockHour < currentHour) {
           $(this).removeClass("present future").addClass("past");
         } else if (timeblockHour === currentHour) {
@@ -20,11 +51,6 @@ $(document).ready(function () {
         }
       });
     }
-  
-    
-    updateTimeblockBackground();
-  
-    
     function loadEventsFromLocalStorage() {
       $(".time-block").each(function () {
         const timeblockId = $(this).attr("id");
@@ -35,6 +61,9 @@ $(document).ready(function () {
         }
       });
     }
+  
+    
+    updateTimeblockBackground();
   
     
     loadEventsFromLocalStorage();
@@ -49,5 +78,11 @@ $(document).ready(function () {
     });
   
     
-    setInterval(updateTimeblockBackground, 60000);
+    function refreshTimeblockColors() {
+      updateTimeblockBackground();
+      setTimeout(refreshTimeblockColors, 60000);
+    }
+  
+    
+    refreshTimeblockColors();
   });
